6f0f1dde8f9e611ebb65d168945d224097b33c05
Toran Sahu <toran.sahu@yahoo.com>
2021-04-23 12:27:53 +0530

    update go template

--- templates/=template=.go
+++ templates/=template=.go
@@ -1,8 +1,9 @@
-//
-// %FFILE%
-// Copyright (C) %YEAR% %USER% <%MAIL%>
-//
-// Distributed under terms of the %LICENSE% license.
-//
+/*
+%FFILE%
+Copyright (C) %YEAR% %USER% <%MAIL%>
 
-package %CURRDIRNAME% %HERE%
+Distributed under terms of the %LICENSE% license.
+*/
+
+// Package %CURRDIRNAME% %HERE%
+package %CURRDIRNAME%
